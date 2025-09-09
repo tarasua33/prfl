1527028573eb104531c14822fd7cc35eb1be27d9
tarasua <taras.rutkovskyi@gmail.com>
2025-09-09 20:43:54 +0300

    resolved hight of game description

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -23,7 +23,7 @@ export class Card extends React.Component {
           <div className="absolute left-3 top-3 right-3 text-white rounded-md bg-dark/20">
 
             <h3 className="px-3 py-1 text-lg font-semibold truncate">{item.title}</h3>
-            <p className="px-3 py-1 text-sm opacity-90 mt-1 line-clamp-2">{item.description}</p>
+            <p className="px-3 py-1 text-sm opacity-90 mt-1 line-clamp-7">{item.description}</p>
 
           </div>
           <div className="absolute left-3 bottom-3 right-3 text-white">
